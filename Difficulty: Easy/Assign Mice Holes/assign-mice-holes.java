3dc671e24108d057f4e4de95633c4b8d3d9d4beb
Dhrub Kumar Thakur <dhrubkumarthakur267@gmail.com>
2025-09-09 22:31:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Assign Mice Holes/assign-mice-holes.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int assignHole(int[] mices, int[] holes) {
+        Arrays.sort(mices);
+        Arrays.sort(holes);
+        
+        int maxTime = 0;
+        for (int i = 0; i < mices.length; i++) {
+            maxTime = Math.max(maxTime, Math.abs(mices[i] - holes[i]));
+        }
+        
+        return maxTime;
+    }
+}
