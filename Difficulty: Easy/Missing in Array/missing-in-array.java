29d5e5a2ec44e52bb1076c2885d60a3f1ef2e1b3
Dhrub Kumar Thakur <dhrubkumarthakur267@gmail.com>
2025-10-02 08:27:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing in Array/missing-in-array.java
+++ Difficulty: Easy/Missing in Array/missing-in-array.java
@@ -1,9 +1,25 @@
+// class Solution {
+//     int missingNum(int arr[]) {
+//         int n = arr.length;
+//         long total = 1L * (n + 1) * (n + 2) / 2; // force long before multiply
+//         long sum = 0;
+//         for (int x : arr) sum += x;
+//         return (int)(total - sum);
+//     }
+// }
+
+
 class Solution {
     int missingNum(int arr[]) {
-        int n = arr.length;
-        long total = 1L * (n + 1) * (n + 2) / 2; // force long before multiply
-        long sum = 0;
-        for (int x : arr) sum += x;
-        return (int)(total - sum);
+    int n=arr.length+1;
+    //sum of natural number of n=n+1;
+    long sum1=(long)n*(n+1)/2;
+    long sum2=0;
+    for(int i=0; i<n-1; i++){
+        sum2=sum2+arr[i];
+    }
+    return (int)(sum1-sum2);
     }
 }
+
+
