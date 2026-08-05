f555ae58b873e5a2f30e5d5473a18e0f8aabfef2
Dhrub Kumar Thakur <dhrubkumarthakur267@gmail.com>
2026-08-05 13:15:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Product of Array/product-of-array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long product(long arr[]) {
+
+        long mod = 1000000007;
+        long product = 1;
+
+        for (int i = 0; i < arr.length; i++) {
+            product = (product * arr[i]) % mod;
+        }
+
+        return product;
+    }
+}
