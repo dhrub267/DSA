7bc2f5570c8f77279ab710d3bc17f468ee7f1903
Dhrub Kumar Thakur <dhrubkumarthakur267@gmail.com>
2026-09-03 21:33:41 +0530

    Time: 3 ms (2.07%), Space: 79.9 MB (94.55%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -1,23 +1,37 @@
+// class Solution {
+//     public int peakIndexInMountainArray(int[] arr) {
+
+//         // first and last element peak nahi ho sakte
+//         int low = 1;
+//         int high = arr.length - 2;
+
+//         while (low <= high) {
+//             int mid =(high + low) / 2;
+
+//             if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) {
+//                 return mid;   // peak found
+//             }
+//             else if (arr[mid] > arr[mid - 1] && arr[mid] < arr[mid + 1]) {
+//                 low = mid + 1; // peak right side
+//             }
+//             else {
+//                 high = mid - 1; // peak left side
+//             }
+//         }
+//         return -1;
+//     }
+// }
+
+
+
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-
-        // first and last element peak nahi ho sakte
-        int low = 1;
-        int high = arr.length - 2;
-
-        while (low <= high) {
-            int mid =(high + low) / 2;
-
-            if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) {
-                return mid;   // peak found
-            }
-            else if (arr[mid] > arr[mid - 1] && arr[mid] < arr[mid + 1]) {
-                low = mid + 1; // peak right side
-            }
-            else {
-                high = mid - 1; // peak left side
-            }
+     int n=arr.length;
+     for(int i=1; i<n-1; i++){
+        if(arr[i-1]<arr[i] && arr[i+1]<arr[i]){
+            return i;
         }
+     }  
         return -1;
     }
 }
